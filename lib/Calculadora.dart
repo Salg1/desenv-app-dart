18df1982696741a3f1810a53c410455aeafe4f30
Salg1 <saulloadelino@gmail.com>
2023-08-26 23:36:16 -0300

    Alterado o nome do App

--- lib/Calculadora.dart
+++ lib/Calculadora.dart
@@ -10,7 +10,7 @@ class CalculadoraState extends State<Calculadora> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculadora 1.0"),
+        title: Text("Calculadora 2.0"),
       ),
 
       body: Container(
